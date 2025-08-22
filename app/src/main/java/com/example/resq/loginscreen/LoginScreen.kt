6c908b7516eb1e709067b9b0fde73e11832f4cc5
adit1 <adit10022004@gmail.com>
2025-08-23 00:35:40 +0530

    first commmit

---
+++ app/src/main/java/com/example/resq/loginscreen/LoginScreen.kt
@@ -0,0 +1,14 @@
+package com.example.resq.loginscreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun loginscreen(){
+    Column (){
+
+
+        Text(text = "ResQ")
+    }
+}
