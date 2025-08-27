8b2a6397e8feea359268d6bad3ef0266bc2c0345
adit1 <adit10022004@gmail.com>
2025-08-27 08:14:25 +0530

    imports

--- app/src/main/java/com/example/resq/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/resq/loginscreen/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.resq.loginscreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
