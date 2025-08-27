55f07285414ae503277872ee05360e2de82aff4e
adit1 <adit10022004@gmail.com>
2025-08-27 08:14:03 +0530

    imports

--- app/src/main/java/com/example/resq/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/resq/loginscreen/LoginScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
