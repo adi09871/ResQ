7cca38d6948953d6c1c57cc5c481edc2f3e0e6e4
adit1 <adit10022004@gmail.com>
2025-09-03 22:54:00 +0530

    imports

--- app/src/main/java/com/example/resq/responderloginscreen/Responderloginscreen.kt
+++ app/src/main/java/com/example/resq/responderloginscreen/Responderloginscreen.kt
@@ -13,6 +13,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 
