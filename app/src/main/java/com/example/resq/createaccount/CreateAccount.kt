e86d113422e129c961700a723c945593c9f64a22
adit1 <adit10022004@gmail.com>
2025-09-04 18:00:44 +0530

    create account page

--- app/src/main/java/com/example/resq/createaccount/CreateAccount.kt
+++ app/src/main/java/com/example/resq/createaccount/CreateAccount.kt
@@ -1,23 +1,36 @@
 package com.example.resq.createaccount
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -89,10 +102,10 @@ fun CreateAccount() {
 
                 // full name  Label
                 Text(
-                    text = "Email", fontWeight = FontWeight.Bold
+                    text = "Full name ", fontWeight = FontWeight.Bold
                 )
 
-                var email by remember { mutableStateOf("") }
+                var fullname by remember { mutableStateOf("") }
                 OutlinedTextField(
                     value =fullname,
                     onValueChange = { fullname = it },
@@ -105,12 +118,12 @@ fun CreateAccount() {
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Password Label
+                // phone number  Label
                 Text(
                     text = "Phone number ", fontWeight = FontWeight.Bold
                 )
 
-                var password by remember { mutableStateOf("") }
+                var phonenumber by remember { mutableStateOf("") }
                 OutlinedTextField(
                     value =phonenumber,
                     onValueChange = {phonenumber = it },
@@ -163,7 +176,7 @@ fun CreateAccount() {
                         contentColor = Color.White, containerColor = Color(0xFFE50914)
                     )
                 ) {
-                    Text(text = "Login")
+                    Text(text = "Create Account")
                 }
             }
         }
