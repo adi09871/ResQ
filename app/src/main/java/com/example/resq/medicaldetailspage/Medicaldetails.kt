a8468b2911a5d9afea8d2e7608ccb37052824c6a
adit1 <adit10022004@gmail.com>
2025-09-05 20:06:04 +0530

    imports

--- app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
+++ app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
@@ -3,6 +3,8 @@ package com.example.resq.medicaldetailspage
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
