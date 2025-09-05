f11a897b1f8eaf640bce5641418dd5b74731586e
adit1 <adit10022004@gmail.com>
2025-09-05 16:31:58 +0530

    medical details page

--- app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
+++ app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
@@ -3,8 +3,13 @@ package com.example.resq.medicaldetailspage
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Medicaldetails
-            () {
+fun Medicaldetails()
+
+{ Column(
+    modifier = Modifier
+        .fillMaxSize()
+        .background(color = pink1)
+        .padding(bottom = 300.dp),
 
     
 }
