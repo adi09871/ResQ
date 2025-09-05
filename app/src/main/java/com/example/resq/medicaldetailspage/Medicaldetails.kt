e89c25acaf31606e6debddab6fcc5a45d9b9e86b
adit1 <adit10022004@gmail.com>
2025-09-05 16:34:28 +0530

    imports

--- app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
+++ app/src/main/java/com/example/resq/medicaldetailspage/Medicaldetails.kt
@@ -1,7 +1,15 @@
 package com.example.resq.medicaldetailspage
 
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.resq.R
+import com.example.resq.ui.theme.pink1
+@Preview
 @Composable
 fun Medicaldetails()
 
