414212dcaf889b434943d25aec0f9eeca70b37f5
adit1 <adit10022004@gmail.com>
2025-09-06 17:28:01 +0530

    qr page

---
+++ app/src/main/java/com/example/resq/Qrpage/qrpage.kt
@@ -0,0 +1,53 @@
+package com.example.resq.Qrpage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.resq.R
+
+@Composable
+@Preview
+fun Qrpage() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFE9FDF1))
+    ) {
+        Row {
+
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp) .padding(top = 8.dp, start = 6.dp)
+            )
+            Text(
+                text = "ResQ ",
+                color = Color(0xFF008C3D),
+                modifier = Modifier
+                    .padding(top = 12.dp, ), fontSize = 18.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text("Profile", modifier = Modifier.padding(top = 12.dp, end = 16.dp), fontSize = 16.sp)
+        }
+    }
+}
