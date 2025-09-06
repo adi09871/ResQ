b511988a3fd1ebb71aed976110eaebe44055704d
adit1 <adit10022004@gmail.com>
2025-09-06 18:25:18 +0530

    imports

--- app/src/main/java/com/example/resq/Qrpage/qrpage.kt
+++ app/src/main/java/com/example/resq/Qrpage/qrpage.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.resq.R
+import com.example.resq.ui.theme.pink1
 
 @Composable
 @Preview
