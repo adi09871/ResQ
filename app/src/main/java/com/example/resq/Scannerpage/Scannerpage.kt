d7c9e711dc04d1f272aa801b1f40d3897648ab03
adit1 <adit10022004@gmail.com>
2025-09-08 22:45:13 +0530

    imports

--- app/src/main/java/com/example/resq/Scannerpage/Scannerpage.kt
+++ app/src/main/java/com/example/resq/Scannerpage/Scannerpage.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.resq.R
 import com.example.resq.ui.theme.pink1
