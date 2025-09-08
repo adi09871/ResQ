234f57dede24cedc554867af392d107a95e5b9ce
adit1 <adit10022004@gmail.com>
2025-09-08 22:43:39 +0530

    changes

--- app/src/main/java/com/example/resq/MainActivity.kt
+++ app/src/main/java/com/example/resq/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.resq.loginscreen.Loginscreen
-import com.example.resq.medicaldetailspage.Medicaldetails
-import com.example.resq.ui.theme.ResQTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/resq/Scannerpage/Scannerpage.kt
@@ -0,0 +1,40 @@
+package com.example.resq.Scannerpage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.resq.R
+import com.example.resq.ui.theme.pink1
+
+@Composable ()
+fun Scannerpage() {
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(color = pink1)){
+
+        Row {
+
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(top = 8.dp, start = 6.dp)
+            )
+
+
+    }
+
+
+
+
+}}
+
