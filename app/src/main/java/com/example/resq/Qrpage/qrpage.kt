59297955a2e6fea3e341aa60175e1a12f4e0e4f6
adit1 <adit10022004@gmail.com>
2025-09-08 22:29:30 +0530

    changes

--- app/src/main/java/com/example/resq/Qrpage/qrpage.kt
+++ app/src/main/java/com/example/resq/Qrpage/qrpage.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -63,6 +64,7 @@ fun Qrpage() {
                     }, fontSize = 16.sp
             )
         }
+
         Spacer(modifier = Modifier.size(20.dp))
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
