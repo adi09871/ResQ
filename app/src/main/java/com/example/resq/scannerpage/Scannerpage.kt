36d0878cbab8d94cc966cf1db04e386ccfa3d404
adit1 <adit10022004@gmail.com>
2025-09-09 09:23:52 +0530

    changes

--- app/src/main/java/com/example/resq/scannerpage/Scannerpage.kt
+++ app/src/main/java/com/example/resq/scannerpage/Scannerpage.kt
@@ -3,6 +3,7 @@ package com.example.resq.scannerpage
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
