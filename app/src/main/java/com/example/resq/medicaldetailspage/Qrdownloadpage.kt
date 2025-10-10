fb87312bbff70f667f9c79b272c5ffb1bc3be205
adit1 <adit10022004@gmail.com>
2025-10-10 18:04:51 +0530

    ignore krne layak h

--- app/src/main/java/com/example/resq/medicaldetailspage/Qrdownloadpage.kt
+++ app/src/main/java/com/example/resq/medicaldetailspage/Qrdownloadpage.kt
@@ -1,7 +1,9 @@
 package com.example.resq.medicaldetailspage
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
