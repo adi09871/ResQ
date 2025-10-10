586d1d6420658ee0dd0ddf0cc2433ceaecb62d6f
adit1 <adit10022004@gmail.com>
2025-10-10 18:05:10 +0530

    imports

--- app/src/main/java/com/example/resq/medicaldetailspage/Qrdownloadpage.kt
+++ app/src/main/java/com/example/resq/medicaldetailspage/Qrdownloadpage.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.resq.R
 import com.example.resq.ui.theme.pink1
 
